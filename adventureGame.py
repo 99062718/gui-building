540b9132d594a216391d14e7675bcd822f43830b
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2021-12-22 14:21:27 +0100

    added beta options menu and buttons that can have their own functions

--- adventureGame.py
+++ adventureGame.py
@@ -1,5 +1,6 @@
 import tkinter
 from tkinter import ttk, IntVar, StringVar, messagebox
+from tkinter.constants import ANCHOR, OUTSIDE
 import random
 
 mainWindow = tkinter.Tk()
@@ -9,7 +10,6 @@ characters = ["hero"]
 maxHealth = 15
 health = 15
 damageMultiplier = 1
-playerAnswer = ""
 
 content = [[],[]]
 
@@ -17,23 +17,30 @@ content = [[],[]]
 # remake room system from original file to be more dynamic and easier to work with,
 # make options menu in gui
 
-def theContentDestroyer9000(removeAll="no"): # theContentDestroyer9000 is back in stock yet again. Now for 30% off for you and your family to destroy content with during the holidays!
+def theContentDestroyer9000(removeAll=False): # theContentDestroyer9000 is back in stock yet again. Now for 30% off for you and your family to destroy content with during the holidays!
     global content
     for box in content[0]:
         box.destroy()
     content[0] = []
-    if removeAll == "yes":
+    if removeAll:
         for box in content[1]:
             box.destroy()
         content[1] = []
 
-def contentCreator(newContent=[], gridOrPack="grid"):
-    global content
+def contentCreator(newContent=[]):
+    global content, playerAnswer
     num = [0, 0]
+    functionToUse = []
 
     theContentDestroyer9000()
 
     for info in newContent:
+
+        try:
+            gridOrPlace = info[2][0]
+        except:
+            gridOrPlace = "grid"
+
         for value in info[1]:
             if info[0] == "label":
                 content[0].append(tkinter.Label(text=value))
@@ -51,22 +58,39 @@ def contentCreator(newContent=[], gridOrPack="grid"):
                     variable=value
                 ))
             elif info[0] == "button":
-                functionToUse = info[2]
-                content[0 if gridOrPack == "grid" else 1].append(tkinter.Button(
+                functionToUse = value
+
+                content[1 if gridOrPlace == "place" else 0].append(tkinter.Button(
                     text=value,
-                    command=lambda: functionFinder(functionToUse)
+                    command = lambda toUse = functionToUse : funcExecute(toUse)
                 ))
             else:
                 raise Exception("Widget provided is not a valid widget type")
 
-            if gridOrPack == "grid":
-                content[0][num].grid(row=num)
-                num[0] += 1
-            else:
-                content[1][num].pack()
+            if gridOrPlace == "place":
+                content[1][num[1]].place(bordermode=OUTSIDE, anchor=info[2][1])
                 num[1] += 1
+            else:
+                content[0][num[0]].grid(row=num[0])
+                num[0] += 1
 
-def functionFinder(functionToUse)
+def funcExecute(functionToUse):
+    functionList[functionToUse]()
+
+def options():
+    theContentDestroyer9000(True)
+    contentCreator([["button", ["Exit"]]])
+
+def exit():
+    theContentDestroyer9000()
+    contentCreator([["button", ["Options"], ["place", "nw"]]])
+
+functionList = {
+    "Options": options,
+    "Exit": exit
+}
+
+contentCreator([["button", ["Options"], ["place", "nw"]]])
             
 
 mainWindow.mainloop()
